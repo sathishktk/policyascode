2871a4c66153c90d73c2bb503ddc64998de44699
sathishktk <35387416+sathishktk@users.noreply.github.com>
2020-04-20 10:32:32 -0400

    Update s3main.tf

--- s3main.tf
+++ s3main.tf
@@ -39,8 +39,12 @@ resource "aws_s3_bucket" "bucket" {
       }
     }
   }
+      
+    tags = {
+    name             = "Sentinel-Test-Bucket"
+    Owner            = "demouser@sathish.com"
+  }  
  
-
 }
 
 output "sse_algorithm" {
